50192e45dfa3d6f3044a771ded668770488365e2
LordCookis <azat525210@gmail.com>
2026-01-29 02:27:28 +0500

    Done

--- frontend-test/postcss.config.mjs
+++ frontend-test/postcss.config.mjs
@@ -1,9 +1,7 @@
-import { defineConfig } from "tailwindcss";
-
-export default defineConfig({
-  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
+const config = {
+  plugins: {
+    "@tailwindcss/postcss": {},
   },
-  plugins: [],
-});
+};
+
+export default config;
